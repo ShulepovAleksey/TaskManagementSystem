3f3c385ef2a11da7c5ff23bf155accb35fe32499
LightHouse <PermLexx@Inbox.ru>
2025-01-28 13:07:58 +0500

    feat #000: разобраться с psr-4

--- web/index.php
+++ web/index.php
@@ -7,11 +7,6 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-// TODO Разобраться с PSR-4 и автозагрузкой классов
-require __DIR__ . '/../components/AbstractProvider.php';
-require __DIR__ . '/../components/Database/DatabaseProvider.php';
-require __DIR__ . '/../components/Database/models/TaskDB.php';
-
 $config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
