5cd8d41d2efa9bcee8afe9ca05ebb81c2455e0b9
LightHouse <PermLexx@Inbox.ru>
2025-01-29 11:00:55 +0500

    feat #000: добавить в шаблон ссылку на модуль "Задачи"

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -7,8 +7,8 @@ use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\Response;
 use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
+use models\LoginForm;
+use models\ContactForm;
 
 class SiteController extends Controller
 {
